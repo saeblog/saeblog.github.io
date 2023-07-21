c58132b21f1414538672747247319890c0512928
Yogi Sbg <yogisbg@gmail.com>
2023-07-21 17:50:47 +0700

    Add files via upload

---
+++ src/ism/js/kang-sae-feed.js
@@ -0,0 +1,21 @@
+var multiFeed = {
+    feedsUri: [
+        {
+            name: "blogger",
+            url: "https://www.sae.icu",
+            tag: ""
+        }
+    ],
+    numPost: 4,
+    showThumbnail: true,
+    showSummary: false,
+    summaryLength: 80,
+    titleLength: "auto",
+    thumbSize: 72,
+    newTabLink: true,
+    containerId: "feed-list-container",
+    readMore: {
+        text: "Selengkapnya",
+        endParam: "?max-results=8"
+    }
+};
